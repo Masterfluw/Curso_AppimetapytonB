de8a57a6be6d84cdf8bd8a7c153329f2ce7e3465
unknown <masterfluw@gmail.com>
2026-03-09 13:11:12 -0500

    WEBHOOK7

--- app.py
+++ app.py
@@ -84,8 +84,8 @@ def recibir_mensajes(req):
                if "text" in messages:
                    text = messages["text"]["body"]
                    numero = messages["from"]
-                          
-                  enviar_mensajes_whatsapp(text, numero)
+                                             
+                   enviar_mensajes_whatsapp(text, numero)
                
        return jsonify({'messaje':'EVENT_RECEIVED'})
     except Exception as e:
